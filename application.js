21fc93b08fadd3982db581599159073afbfe44ca
AmyOhMy <amylmihal@gmail.com>
2014-04-05 14:26:18 -0500

    Added more if/else messages

--- application.js
+++ application.js
@@ -1,20 +1,35 @@
 $(document).ready(function() {
-	var answer = Math.random;
-	Math.random <= 100;
-	alert(answer);
+	var randomNumber = Math.floor(Math.random()*100);
+	alert(randomNumber);
+
 	$('button').click(function() {
 		var guess = $('input').val();
+
 		$('ul').prepend(guess);
-		if(guess == answer) {
-			alert("You're correct!");
-		} else if((Math.abs(guess-answer))<5){
-			alert("hot");
-		} else if ((Math.abs(guess-answer))<10){
-			alert("hot");
-		} else if ((Math.abs(guess-answer))<15){
-			alert("hot");
-		} 
-	}
+		if (guess == NaN) {
+			alert("Numbers only please!");
+		} else if(guess > 100) {
+			alert("Keep it below 100 please!");
+		} else if(guess < 1) {
+			alert("Let's keep it positive!");	
+		} else if(guess == randomNumber) {
+			alert("You guessed correct!");	
+		} else if((Math.abs(guess-randomNumber))<2){
+			alert("Burning hot!");	
+		} else if((Math.abs(guess-randomNumber))<5){
+			alert("Hot!");
+		} else if ((Math.abs(guess-randomNumber))<10){
+			alert("Warm");
+		} else if ((Math.abs(guess-randomNumber))<15){
+			alert("Luke Warm");
+		} else if ((Math.abs(guess-randomNumber))<20){
+			alert("Cold");
+		} else if ((Math.abs(guess-randomNumber))<25){
+			alert("Freezing Cold!");
+		} else if((Math.abs(guess-randomNumber))<100){
+			alert("Total popsicle!");	
+		
+				
+	}	
 }
 
-/*var answer = Math.floor(Math.random()*100); Try this on lines 2,3. */
